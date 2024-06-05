570f33a48c8668d0571f2bbcbe6aac1bacb10733
Saidorifkhuja Yuldoshkhujaev <yoldoshxojayevsaidorifxoja@gmail.com>
2024-06-05 17:32:12 +0500

    digital library

--- core/settings.py
+++ core/settings.py
@@ -54,15 +54,19 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+
 ]
 
 AUTH_USER_MODEL = "accounts.User"
 ROOT_URLCONF = 'core.urls'
+CORS_ALLOW_ALL_ORIGINS = True
 
 TEMPLATES = [
     {
