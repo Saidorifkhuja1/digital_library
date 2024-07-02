ab8264f92582bd4d9f82d7ea07570b152a77596c
Saidorifkhuja Yuldoshkhujaev <yoldoshxojayevsaidorifxoja@gmail.com>
2024-07-02 20:02:46 +0500

    digital library

---
+++ accounts/management/commands/delete_expired_users.py
@@ -0,0 +1,19 @@
+# accounts/management/commands/delete_expired_users.py
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+from accounts.models import User
+
+class Command(BaseCommand):
+    help = 'Delete users whose deletion date has passed'
+
+    def handle(self, *args, **kwargs):
+        now = timezone.now()
+        users_to_delete = User.objects.filter(deletion_date__lte=now)
+        count = users_to_delete.count()
+        users_to_delete.delete()
+        self.stdout.write(self.style.SUCCESS(f'Successfully deleted {count} users'))
+
+
+
+
+
